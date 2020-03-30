a45b7182c3b457e3608e255a44f2b2cf6234fbcd
Carsten Daurehøj <arelstone@gmail.com>
2020-03-30 11:25:47 +0200

    create project

--- src/commands/project/create.ts
+++ src/commands/project/create.ts
@@ -1,36 +1,55 @@
-import { Command, flags } from '@oclif/command'
-import axios from 'axios'
-import { axiosOptions } from '../../get-auth-token'
+import {
+    Command,
+    flags,
+} from '@oclif/command'
+import fetch from 'node-fetch'
+import {
+    BASE_URL,
+    headers,
+} from '../../helpers/fetch'
 
 export default class ProjectCreate extends Command {
-  static description = 'Create a new project'
+    static description = 'Create a new project'
 
-  static flags = {
-    help: flags.help({ char: 'h' }),
-    name: flags.string({ char: 'n', required: true }),
-    description: flags.string({ char: 'd' }),
-  }
-
-  static examples = [
-    '$ tradoura project:create -n=<name> -d=<decription>',
-    '$ tradoura project:create --name=myNewUberCoolProject',
-    '$ tradoura project:create --name=myNewUberCoolProject --description="My cool translation project"',
-  ]
-
-  async run() {
-    const { flags: { name, description } } = this.parse(ProjectCreate)
-
-    this.log(name, description)
-
-    const body = { name, description }
-
-    const { data, status, error } = await axios.post('/projects', body, axiosOptions())
-    if (status <= 200 || status > 300) {
-      this.log(`Project ${data.name} created (${data.id})`)
-    } else {
-      this.log(error)
-      this.warn('Could not create project')
-      this.log(error)
+    static flags = {
+        help: flags.help({
+            char: 'h',
+        }),
+        name: flags.string({
+            char: 'n',
+            required: true,
+            helpValue: 'The name of the project',
+        }),
+        description: flags.string({
+            char: 'd',
+            helpValue: 'The description of the project',
+        }),
     }
-  }
+
+    static examples = [
+        '$ tradoura project:create -n=<name> -d=<decription>',
+        '$ tradoura project:create --name="My Project"',
+        '$ tradoura project:create --name="My Project" --description="My cool translation project"',
+    ]
+
+    async run() {
+        const {
+            flags: {
+                name, description,
+            },
+        } = this.parse(ProjectCreate)
+
+        const response: any = await fetch(`${BASE_URL}/projects`, {
+            method: 'POST',
+            body: JSON.stringify({
+                name,
+                description,
+            }),
+            headers,
+        })
+            .then(r => r.json())
+            .catch(error => this.error(error))
+
+        this.log(`Project ${response.data.name} was created (id: ${response.data.id})`)
+    }
 }
---
+++ src/helpers/fetch.ts
@@ -0,0 +1,6 @@
+export const BASE_URL = 'http://localhost:8080/api/v1'
+
+export const headers = {
+  'content-type': 'application/json',
+  authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ODVmZTY3ZS02YTU3LTQ2OGYtYTJjZC02OTM4M2Q0ZjNiODciLCJ0eXBlIjoidXNlciIsImlhdCI6MTU4NTU1ODQ5NiwiZXhwIjoxNTg1NjQ0ODk2fQ.8j12ZdqK7hHwOzCeid17wc4JinzNNY-3pY3ZhZUMHOo',
+}
