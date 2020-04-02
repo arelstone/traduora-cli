059cad4e6958e261a97626571f24595d161b7dac
Carsten Daurehøj <arelstone@gmail.com>
2020-04-02 14:07:21 +0200

    fixed @types/jest and @types/mocha conflict

--- src/helpers/__tests__/flags.spec.ts
+++
@@ -1,9 +0,0 @@
-import { helpFlag } from '../flags';
-
-describe('flags', () => {
-    describe('helpFlag', () => {
-        it('should have h as char', () => {
-            expect(helpFlag().char).toEqual('h');
-        });
-    });
-});
