63f0262a9a0b06823f4b09773b9f89542844172b
Carsten Daurehøj <arelstone@gmail.com>
2020-04-26 09:21:57 +0200

    commit

--- scripts/prepack.ts
+++ scripts/prepack.ts
@@ -1,7 +1,7 @@
 import { exec } from 'shelljs';
 
 exec('cat ./docs/changelog.md > CHANGELOG.md')
-exec('git add . && git commit --amend')
+exec('git add . && git commit --amend --no-edit')
 exec('rm -rf lib && tsc -b')
 exec('oclif-dev manifest')
 exec('oclif-dev readme')
